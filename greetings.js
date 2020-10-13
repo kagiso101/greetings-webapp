e684e51b88e172f2d562b604d59c99c8cb6b087a
kagiso101 <hadebekagiso3@gmail.com>
2020-10-13 10:49:18 +0200

    l ;

--- greetings.js
+++ greetings.js
@@ -2,34 +2,34 @@ module.exports = function (pool) {
 
 
 
-     function greetUser(name, language) {
+    function greetUser(name, language) {
+      
+            switch (language) {
 
-        switch (language) {
+                case "english":
+                    return "Hello, " + name;
+                case "Spanish":
+                    return "Hola, " + name;
 
-            case "english":
-                return "Hello, " + name;
-            case "Spanish":
-                return "Hola, " + name;
-
-            case "French":
-                return "Bonjour , " + name;
+                case "French":
+                    return "Bonjour , " + name;
         }
-
     }
 
 
     //adds to db
     async function verifyName(name) {
-        var regularExpression = /[^A-Za-z]/g;
-        var lettersOnly = name.replace(regularExpression, "")
-        var fixedName = lettersOnly.charAt(0).toUpperCase() + lettersOnly.slice(1).toLowerCase()
-
-        
+        if (!name == "") {
+            var regularExpression = /[^A-Za-z]/g;
+            var lettersOnly = name.replace(regularExpression, "")
+            var fixedName = lettersOnly.charAt(0).toUpperCase() + lettersOnly.slice(1).toLowerCase()
             const checking = await pool.query(`select id from greetings where name = $1`, [fixedName])
             if (checking.rowCount === 0) {
                 await pool.query(`insert into greetings (name, greeted) values ($1, 0)`, [fixedName]);
             }
             await pool.query(`update greetings set greeted = greeted+1 where name = $1`, [fixedName])
+        }
+
     }
     //gets counter for all greeted users
     async function greetCount() {
